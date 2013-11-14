151ad2a3767c5e902fc15ac96087ec920b9cb060
Ernad Husremovic <hernad@bring.out.ba>
2013-11-14 08:19:44 +0100

    .bash_profile

--- init_home.sh
+++ init_home.sh
@@ -2,3 +2,7 @@
 git config --global user.email "hernad@bring.out.ba"
 git config --global user.name "Ernad Husremovic"
 
+echo "PATH=\~/bin:\$PATH" > ~/.bash_profile
+
+
+echo "source ~/bash_profile" >> ~/.bashrc
