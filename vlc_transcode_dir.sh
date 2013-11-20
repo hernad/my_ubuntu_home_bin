bec7102c74e4ba386091a48a5b069962d8506ebb
Ernad Husremovic <hernad@bring.out.ba>
2013-11-20 14:24:45 +0100

    acodec ne mijenjaj kod transcode-a

--- vlc_transcode_dir.sh
+++ vlc_transcode_dir.sh
@@ -10,9 +10,9 @@ dst="trans"
 
 mkdir $dst
 
-for a in *$fmt; do
+for a in *; do
 echo $a
-$vlc -I dummy -vvv "./$a" --sout "#transcode{vcodec=h264,vb=0,scale=0,acodec=mpga,ab=128,channels=2}:standard{mux=$mux,dst=$dst/$a.$ext,access=file}" vlc://quit
+$vlc -I dummy -vvv "./$a" --sout "#transcode{vcodec=h264,vb=0,scale=0,ab=128,channels=2}:standard{mux=$mux,dst=$dst/$a.$ext,access=file}" vlc://quit
 echo $a
 
 done
