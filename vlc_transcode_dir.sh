39356a28f976977093173d0b77531e28a21d6b57
Ernad Husremovic <hernad@bring.out.ba>
2013-11-20 16:45:33 +0100

    update trans

--- vlc_transcode_dir.sh
+++ vlc_transcode_dir.sh
@@ -1,4 +1,4 @@
-vcodec="h264"
+vcodec="mp4v"
 acodec="mp4a"
 bitrate="1024"
 arate="192"
@@ -10,9 +10,9 @@ dst="trans"
 
 mkdir $dst
 
-for a in *; do
+for a in *\.*; do
 echo $a
-$vlc -I dummy -vvv "./$a" --sout "#transcode{vcodec=h264,vb=0,scale=0,ab=128,channels=2}:standard{mux=$mux,dst=$dst/$a.$ext,access=file}" vlc://quit
+$vlc -I dummy $a --sout "#transcode{vcodec=h264,vb=0,scale=0,ab=128,channels=2}:standard{mux=$mux,dst=$dst/$a.$ext,access=file}" vlc://quit
 echo $a
 
 done
