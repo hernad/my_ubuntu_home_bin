af605dda36e6d97005e5ac5b5cd03848a5c5643e
Ernad Husremovic <hernad@bring.out.ba>
2013-11-27 15:21:21 +0100

    debug

--- vlc_rotate_dir.sh
+++ vlc_rotate_dir.sh
@@ -9,8 +9,32 @@ dst="rotated"
 
 mkdir $dst
 
-for a in *3gp; do
-$vlc -I dummy -vvv "./$a" --sout "#transcode{vcodec=h264,vb=0,scale=0,acodec=mpga,ab=128,channels=2}:standard{mux=$mux,dst=$dst/$a.$ext,access=file}" --sout-transcode-vfilter transform --transform-type vflip --sout-keep vlc://quit
-echo $a
-done
+rotate() {
 
+if [ "$1" == "h" ] ; then
+   echo "Rotiram samo horizontalno"
+   TRAN="--transform-type hflip"
+else
+   TRAN="--transform-type hflip --transform-type vflip"
+fi
+  
+$vlc -I dummy  -v $a --sout "#transcode{vcodec=h264,vb=0,scale=0,ab=128,channels=2}:standard{mux=mp4,dst=$dst/$a.$ext,access=file}" --sout-transcode-vfilter transform $TRAN --sout-keep vlc://quit
+
+echo ROTATE $a END
+}
+
+
+mkdir $dst
+
+for a in *\.*; do
+
+  echo $a
+
+  rm $dst/$a.$ext
+
+  # iz nekog razloga vlc jednostavno ne uradi posao, a kod novog pokretanja uradi !?
+  until [ -f $dst/$a.$ext ] ; do
+    rotate $1
+  done
+
+done
--- vlc_transcode_dir.sh
+++ vlc_transcode_dir.sh
@@ -4,15 +4,39 @@ bitrate="1024"
 arate="192"
 ext="mp4"
 mux="ts"
-vlc="vlc"
+vlc="cvlc"
 fmt="3gp"
 dst="trans"
+
+transcode() {
+$vlc  -v $a --sout "#transcode{vcodec=h264,vb=0,scale=0,ab=128,channels=2}:standard{mux=mp4,dst=$dst/$a.$ext,access=file}" vlc://quit
+
+echo TRANSCODE $a END
+}
+
 
 mkdir $dst
 
 for a in *\.*; do
+
 echo $a
-$vlc -I dummy $a --sout "#transcode{vcodec=h264,vb=0,scale=0,ab=128,channels=2}:standard{mux=$mux,dst=$dst/$a.$ext,access=file}" vlc://quit
+
+rm $dst/$a.$ext
+
+# iz nekog razloga vlc jednostavno ne uradi posao, a kod novog pokretanja uradi !?
+until [ -f $dst/$a.$ext ] ; do
+  transcode
+done
+
+#ovo ne pije vode exit code je uvijek 0 kod vlc-a :(
+#if [ ! "$?" == "0" ] ; then
+#   echo ERR transcode $a neuspjesan !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#
+#   exit 1
+#else
+#   echo transcode uspjesan
+#fi
+
 echo $a
 
 done
