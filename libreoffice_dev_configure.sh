28ad144ce30311aeb415c4d86d410d052418b74a
Ernad Husremovic <hernad@bring.out.ba>
2014-02-06 13:18:51 +0100

    SAL_USE_VCLPLUGIN

--- libreoffice_dev_configure.sh
+++ libreoffice_dev_configure.sh
@@ -4,3 +4,8 @@ export LD_LIBRARY_PATH=
 
 ./configure --without-java --enable-gtk3 --enable-static-gtk --prefix=/opt/knowhowERP/LO --without-doxygen --with-system-cairo --disable-gstreamer-0-10 --without-help --without-myspell-dicts --disable-extensions --disable-python --disable-odk
 
+
+
+echo setuj environment varijable:
+echo SAL_USE_VCLPLUGIN=gtk3 GDK_BACKEND=broadway /opt/knowhowERP/LO/program/soffice -calc
+
