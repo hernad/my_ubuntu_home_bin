6930858cd181eeb2eaa06cd89fe11b615a14d0a8
Ernad Husremovic <hernad@bring.out.ba>
2014-02-06 13:13:47 +0100

    skia documentation

--- skia_download.sh
+++ skia_download.sh
@@ -5,3 +5,5 @@ gclient config --name . https://skia.googlesource.com/skia.git
 gclient sync
 git checkout master
 
+xdg-open https://sites.google.com/site/skiadocs/user-documentation/quick-start-guides/linux
+
