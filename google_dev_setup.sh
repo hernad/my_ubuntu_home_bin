5defa4966dc9ab83aee3efd2cc02edf6ba7e96ef
Ernad Husremovic <hernad@bring.out.ba>
2014-02-06 16:48:17 +0100

    build chromium

--- google_dev_setup.sh
+++ google_dev_setup.sh
@@ -5,3 +5,10 @@ sudo apt-get install libpng12-0 libpng12-dev
 sudo apt-get install libfreetype6 libfreetype6-dev libfontconfig libfontconfig-dev
 sudo apt-get install libgif-dev
 sudo apt-get install freeglut3-dev
+
+
+echo chromium
+sudo apt-get install libpci-dev libelf-dev libcap-dev
+
+xdg-open https://code.google.com/p/chromium/wiki/LinuxBuildInstructions#Compilation
+
